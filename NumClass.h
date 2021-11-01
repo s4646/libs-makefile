ee644a0252ec8175e71d8a8369d6e2ffd6135030
s4646 <sahar45456@gmail.com>
2021-11-01 19:41:05 +0200

    Added NumClass.h

---
+++ NumClass.h
@@ -0,0 +1,12 @@
+#ifndef NUMCLASS_H_INCLUDED
+#define NUMCLASS_H_INCLUDED
+
+int isArmstrong(int);
+
+int isPalindrome(int);
+
+int isPrime(int);
+
+int isStrong(int);
+
+#endif // NUMCLASS_H_INCLUDED

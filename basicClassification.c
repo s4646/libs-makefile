4ccab869c0c891319822ac97d6a650088d5d88ce
s4646 <sahar45456@gmail.com>
2021-11-03 21:16:31 +0200

    Modified basicClassification.c
    Added 1 as prime number

--- basicClassification.c
+++ basicClassification.c
@@ -19,8 +19,11 @@ int isStrong(int n) {
 }
 
 int isPrime(int n) {
-    if (n <= 1) {
+    if (n < 1) {
         return 0;
+    }
+    if(n==1) {
+        return 1;
     }
     for (int i = 2; i < n; i++) {
         if (n % i == 0) {
